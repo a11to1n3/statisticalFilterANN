6745c3423c714e00b027e681e762991ddd190b89
Duy Anh Pham <duyanhpham@outlook.com>
2019-12-10 09:47:39 +0100

    Change PCA config

--- dataPCA.py
+++ dataPCA.py
@@ -121,8 +121,6 @@ def plotPCAandSplit(data_all):
     
     
     # Plot PCA
-    figure = plt.figure()
-    figure(figsize=(15,10))
     plt.scatter(mon[:,0],mon[:,1],'o',label="Change from Sundays to Mondays")
     plt.scatter(sun[:,0],sun[:,1],'v',label="Change from Saturdays to Sundays")
     plt.scatter(rests[:,0],rests[:,1],'s',label="The rests")
