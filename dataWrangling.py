96b64a0147d7847e00fe864759350ffc06ee51f0
duyanhpham@outlook.com <duyanhpham@outlook.com>
2019-12-22 16:07:12 +0100

    Fixed Date data format

--- dataWrangling.py
+++ dataWrangling.py
@@ -209,7 +209,8 @@ def csvToArray(filename):
         if len(df.values[i,1]) > 7:
             d = int(df.values[i,1][-8:-6])+115
         else:
-           d = int(df.values[i,1][-7:-6])+115
+            print(df.values[i,1])
+            d = int(df.values[i,1][-7:-6])+115
         data[i,0,d] = 1
         data[i,1,d] = 1
         data[i,2,d] = 1
